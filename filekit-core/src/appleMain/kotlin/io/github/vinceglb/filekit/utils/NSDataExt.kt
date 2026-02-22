4ae653aabea3bc76b8e6b93942212ad3bb0c1147
vinceglb <vincent.guillebaud31@gmail.com>
2026-02-22 16:53:16 +0100

    🚑️ Fix project build 4

--- filekit-core/src/appleMain/kotlin/io/github/vinceglb/filekit/utils/NSDataExt.kt
+++ filekit-core/src/appleMain/kotlin/io/github/vinceglb/filekit/utils/NSDataExt.kt
@@ -1,13 +1,9 @@
 package io.github.vinceglb.filekit.utils
 
-import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.UnsafeNumber
-import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.refTo
-import kotlinx.cinterop.usePinned
 import platform.Foundation.NSData
-import platform.Foundation.create
 import platform.posix.memcpy
 
 @OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
@@ -17,10 +13,12 @@ public fun NSData.toByteArray(): ByteArray = let { nsData ->
     }
 }
 
-@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class, UnsafeNumber::class)
-public fun ByteArray.toNSData(): NSData = usePinned {
-    NSData.create(
-        bytes = it.addressOf(0),
-        length = this.size.toULong(),
-    )
-}
+public expect fun ByteArray.toNSData(): NSData
+
+// @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class, UnsafeNumber::class)
+// public fun ByteArray.toNSData(): NSData = usePinned {
+//    NSData.create(
+//        bytes = it.addressOf(0),
+//        length = this.size.toULong(),
+//    )
+// }
